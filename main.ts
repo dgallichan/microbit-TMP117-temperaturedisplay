1589737dfbf0059ef4634f9acaa02ba69554ac53
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-07 09:39:35 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let tempC = 0
+Kitronik_VIEWTEXT32.clearDisplay()
+Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "Hello!")
+basic.forever(function () {
+    Kitronik_VIEWTEXT32.clearDisplay()
+    tempC = pins.i2cReadNumber(72, NumberFormat.Int16BE, false)
+    tempC = tempC * 0.0078125
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "Temperature: " + convertToText(tempC) + " C")
+    basic.pause(500)
+})
