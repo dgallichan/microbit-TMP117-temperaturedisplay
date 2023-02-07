2e0abe1c5cb4ff6d923459f88bb1d4cf1bae80e4
Daniel Gallichan <dgallichan@yahoo.com>
2023-02-07 09:42:43 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ basic.forever(function () {
     Kitronik_VIEWTEXT32.clearDisplay()
     tempC = pins.i2cReadNumber(72, NumberFormat.Int16BE, false)
     tempC = tempC * 0.0078125
-    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "Temp: " + convertToText(tempC) + " C")
-    basic.pause(500)
+    Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, "Temp: " + convertToText(tempC).substr(0, 5) + " C")
+    basic.pause(100)
+    serial.writeValue("tempC", tempC)
 })
